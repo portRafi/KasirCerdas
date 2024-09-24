b1bc7480469fd280d47b903a3c0edc8fae0e8656
miiiwdy <fahmiwidyaa@gmail.com>
2024-09-24 13:26:46 +0700

    #3

---
+++ app/Filament/Resources/KeranjangResource.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\KeranjangResource\Pages;
+use App\Filament\Resources\KeranjangResource\RelationManagers;
+use App\Models\Keranjang;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class KeranjangResource extends Resource
+{
+    protected static ?string $model = Keranjang::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('kode')
+                    ->label('Kode Barang'),
+                Tables\Columns\TextColumn::make('kategori')
+                    ->label('Kategori'),
+                Tables\Columns\TextColumn::make('nama')
+                    ->label('Nama Barang'),
+                Tables\Columns\TextColumn::make('quantity')
+                    ->label('Quantity'),
+                Tables\Columns\TextColumn::make('total_harga')
+                    ->label('Total Harga'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListKeranjangs::route('/'),
+            'create' => Pages\CreateKeranjang::route('/create'),
+            'edit' => Pages\EditKeranjang::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/KeranjangResource/Pages/CreateKeranjang.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\KeranjangResource\Pages;
+
+use App\Filament\Resources\KeranjangResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateKeranjang extends CreateRecord
+{
+    protected static string $resource = KeranjangResource::class;
+}
