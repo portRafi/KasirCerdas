936c07616e585b4d84b11dcc1f5d6788b73204a8
rafi <srafi8935@gmail.com.com>
2024-10-02 17:11:40 +0700

    tes

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -10,7 +10,7 @@ class AuthServiceProvider extends ServiceProvider
     /**
      *
      *
-     * @var array<class-string, class-string>
+     * @var array<class-string, 
      */
     protected $policies = [
         'App\Models\Blog\Author' => 'App\Policies\Blog\AuthorPolicy',
