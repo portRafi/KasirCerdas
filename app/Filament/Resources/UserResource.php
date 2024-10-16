767494d2e133675d989dc9a17fa4e194616df98e
miiiwdy <fahmiwidyaa@gmail.com>
2024-10-16 14:46:25 +0700

    test

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -141,12 +141,11 @@ class UserResource extends Resource
         ];
     }
 
-    public static function shouldRegisterNavigation(): bool
-    {
-        if(!auth()->user()->hasRole('super_admin')) {
-            return false;   
-        }
-
-        return true;
-    }
+    // public static function shouldRegisterNavigation(): bool
+    // {
+    //     if(!auth()->user()->hasRole('super_admin')) {
+    //         return false;   
+    //     }
+    //     return true;
+    // }
 }
