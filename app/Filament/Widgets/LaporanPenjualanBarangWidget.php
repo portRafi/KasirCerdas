104fe5dd181cffce68ee0f3ee3ad54ad204f284c
miiiwdy <fahmiwidyaa@gmail.com>
2024-10-23 11:57:22 +0700

    add data laporan transaksi di dashboard panel

--- app/Filament/Widgets/LaporanPenjualanBarangWidget.php
+++ app/Filament/Widgets/LaporanPenjualanBarangWidget.php
@@ -2,31 +2,87 @@
 
 namespace App\Filament\Widgets;
 
-use App\Models\PenjualanBarang;
+use actions;
 use Filament\Tables;
-use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
-use Filament\Widgets\TableWidget as BaseWidget; 
+use App\Models\DataTransaksi;
+use App\Models\PenjualanBarang;
+use Illuminate\Support\Facades\Auth;
+use Filament\Tables\Filters\Filter;
+use Filament\Forms\Components\DatePicker;
+use Illuminate\Database\Eloquent\Builder;
+use Filament\Widgets\TableWidget as BaseWidget;
 
 class LaporanPenjualanBarangWidget extends BaseWidget
 {
+    protected int | string | array $columnSpan = 'full';
     public function table(Table $table): Table
     {
         return $table
             ->query(
-                PenjualanBarang::query()
+                DataTransaksi::where([
+                    ['bisnis_id', '=', Auth::user()->bisnis_id],
+                    ['cabangs_id', '=', Auth::user()->cabangs_id],
+                ])->with('cabang')
             )
             ->columns([
-                TextColumn::make('kode')
-                    ->label('Kode'),
-                TextColumn::make('nama')
-                    ->label('Nama'),
-                TextColumn::make('jumlah')
-                    ->label('Jumlah'),
-                TextColumn::make('total_pendapatan')
-                    ->label('Total Pendapatan'),
-                TextColumn::make('keuntungan')  
-                    ->label('Keuntungan'),
+                Tables\Columns\TextColumn::make('cabang.nama_cabang')
+                    ->label('Nama Cabang')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('kode_transaksi')
+                    ->label('Kode Transaksi')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('email_staff')
+                    ->label('Email Staff')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('total_harga')
+                    ->label('Total Harga')
+                    ->numeric()
+                    ->money('IDR')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('keuntungan')
+                    ->label('Keuntungan')
+                    ->numeric()
+                    ->money('IDR')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                Filter::make('date_range')
+                    ->form([
+                        DatePicker::make('start_date')
+                            ->label('Start Date')
+                            ->required(),
+                        DatePicker::make('end_date')
+                            ->label('End Date')
+                            ->required(),
+                    ])
+                    ->query(function (Builder $query, array $data): Builder {
+                        return $query
+                            ->when(
+                                $data['start_date'],
+                                fn(Builder $query, $date): Builder => $query->whereDate('created_at', '>=', $date)
+                            )
+                            ->when(
+                                $data['end_date'],
+                                fn(Builder $query, $date): Builder => $query->whereDate('created_at', '<=', $date)
+                            );
+                    }),
+            ])
+            ->actions([
+                Tables\Actions\Action::make('view')
+                    ->label('View')
+                    ->icon('heroicon-o-eye')
+                    ->modalHeading('Detail Transaksi')
+                    ->modalContent(function ($record) {
+                        return view('filament.tables.modals.view-transaction', ['record' => $record]);
+                    })
+                    ->color('primary'),
             ]);
     }
 }
