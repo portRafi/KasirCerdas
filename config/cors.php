e5b337c3e8029d6a6598993952a14d81a5a48324
fahmi widya <fahmiwidyaa@gmail.com>
2025-01-10 17:26:05 +0700

    cloudflare r2

--- config/cors.php
+++ config/cors.php
@@ -2,32 +2,27 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
+    'paths' => ['api/*', 'sanctum/csrf-cookie', ''],
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
-    'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['*'],
+    "AllowedMethods" => [
+        "GET",
+        "PUT",
+        "POST",
+        "DELETE"
+    ],
 
     'allowed_origins' => ['http://127.0.0.1:8000'],
-    
+
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
+    'exposed_headers' => ['
+        "x-amz-server-side-encryption",
+        "x-amz-request-id",
+        "x-amz-id-2",
+        "Content-Type"
+    '],
 
     'max_age' => 0,
 
